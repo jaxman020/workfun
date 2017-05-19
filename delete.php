d123b797b0d33b4f1a80430698aac0e858529838
jaxman020 <jaxman020@gmail.com>
2017-05-19 16:36:48 +0800

    5/19
    debug

--- delete.php
+++ delete.php
@@ -4,6 +4,6 @@ session_start();
 //連接資料庫
 require_once("function.php");//引入函數庫
 $member = new member();
-$eid = $_get['eid'];
-$member->delete_e($username);//使用刪除使用者函數
+$eid = $_POST['eid'];
+$member->delete_e($eid);//使用刪除使用者函數
 ?>
